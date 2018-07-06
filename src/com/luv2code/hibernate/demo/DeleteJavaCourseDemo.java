5a72fab11ac283a08d958fef690b616d9cb68311
larry <ladams1776@yahoo.com>
2018-07-05 22:18:44 -0400

    Deleteing the javacourse and not deleting the students (not cascading)

---
+++ src/com/luv2code/hibernate/demo/DeleteJavaCourseDemo.java
@@ -0,0 +1,54 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+import com.luv2code.hibernate.demo.entity.Review;
+import com.luv2code.hibernate.demo.entity.Student;
+
+
+public class DeleteJavaCourseDemo {
+
+	public static void main(String[] args) {
+		final SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.addAnnotatedClass(Course.class)
+				.addAnnotatedClass(Review.class)
+				.addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+		
+		final Session session = factory.openSession();
+		
+		
+			
+		try {
+			session.beginTransaction();
+			
+			
+			
+			// get the Java course from the db
+			int javaCourseId = 15;
+			Course javaCourse = session.get(Course.class, javaCourseId);
+			
+			// delete the course
+			session.delete(javaCourse);
+		
+			// commit
+			session.getTransaction().commit();
+			
+		} catch(Exception exception) {
+			exception.printStackTrace();
+		} finally {			
+			session.close();
+			
+			factory.close();
+		}
+	}
+
+}
